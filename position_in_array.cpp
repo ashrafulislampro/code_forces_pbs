e6c49238e5632d939064b5753873ffeec4c45226
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-10-11 00:07:40 +0600

    position in array

---
+++ position_in_array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int j = 0; j < n; j++)
+    {
+        if (arr[j] <= 10)
+        {
+            cout << "A[" << j << "] = " << arr[j] << endl;
+        }
+    }
+    return 0;
+}
